c8445b4c04be13b0a6ebe9c6a86be10f46dd0e9b
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2018-05-11 14:32:06 +1200

    working ping pong WebSocket implementation

--- blog_websocket/Startup.cs
+++ blog_websocket/Startup.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Threading;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace blog_websocket
 {
     public class Startup
     {
+        private int messageCount = 0;
+
+        private string messageReply(string message)
+        {
+            return string.Format("Message number {0} is {1}", messageCount++, message);
+        }
+
+        private MessageObject defaultMessage(MessageObject messageObject)
+        {
+            return new MessageObject(messageReply(messageObject.Message));
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -18,17 +34,79 @@ namespace blog_websocket
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(LogLevel.Debug);
+            loggerFactory.AddDebug(LogLevel.Debug);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseWebSockets();
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Echo(context, webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });
+
 
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
         }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                string receiveString = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+
+                MessageObject messageObject = JsonConvert.DeserializeObject<MessageObject>(receiveString);
+
+                buffer = System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(defaultMessage(messageObject)));
+
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, buffer.Length), WebSocketMessageType.Text, result.EndOfMessage, CancellationToken.None);
+
+				buffer = new byte[1024 * 4];
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+
     }
+
+    public class MessageObject
+    {
+        public MessageObject(string message){
+            Message = message;
+        }
+
+        public string Message
+        {
+            get;
+            set;
+        }
+    } 
 }
